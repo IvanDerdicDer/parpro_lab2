e0b998a7770e038498679490c676a44dcdf17039
IvanDerdicDer <46329317+IvanDerdicDer@users.noreply.github.com>
2023-05-10 21:41:33 +0200

    Connect 4 works

--- connect_four.py
+++ connect_four.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass, field
 from typing import Optional, Generator
 from enum import Enum
 from copy import deepcopy
+from hashlib import md5
 
 
 class TokenEnum(Enum):
@@ -25,6 +26,11 @@ class ConnectFourBoard:
     is_column_full: Optional[list[bool]] = field(default=None)
     last_token_placed_position: Optional[tuple[int, int]] = field(default=None, init=False)
     last_token_placed: Optional[TokenEnum] = field(default=None, init=False)
+    weight: int = field(default=0, init=False)
+    is_winning: bool = field(default=False, init=False)
+
+    def __hash__(self):
+        return md5(str(self))
 
     def __post_init__(self) -> None:
         if self.width < 7 or self.height < 6:
@@ -48,21 +54,6 @@ class ConnectFourBoard:
     def __repr__(self) -> str:
         return f"ConnectFourBoard(width={self.width}, height={self.height}, board={self.board})"
 
-    # def _inplace_drop_token(self, column: int, token: TokenEnum) -> "ConnectFourBoard":
-    #     if self.is_column_full[column]:
-    #         raise ColumnFullException(f"Column {column} is full")
-    #
-    #     for row in range(self.height - 1, -1, -1):
-    #         if self.board[column][row] is None:
-    #             self.board[column][row] = token
-    #
-    #             self.last_token_placed_position = (column, row)
-    #             self.last_token_placed = token
-    #
-    #             if row == self.height - 1:
-    #                 self.is_column_full[column] = True
-    #             return self
-
     def drop_token(self, column: int, token: TokenEnum) -> "ConnectFourBoard":
         new_board = ConnectFourBoard(self.width, self.height, deepcopy(self.board), self.is_column_full)
         # return new_board._inplace_drop_token(column, token)
@@ -82,55 +73,47 @@ class ConnectFourBoard:
                 return new_board
 
     def is_last_move_winning(self) -> bool:
-        # TODO[Ivan] Write the correct function. From last placed go in all directions.
-
-        if self.last_token_placed is None:
-            return False
+        horizontal_count = 0
+        vertical_count = 0
+        diagonal_left_count = 0
+        diagonal_right_count = 0
 
         column, row = self.last_token_placed_position
 
-        # Check horizontal left
-        if column - 4 > -2:
-            for i in range(column, column - 4, -1):
-                if self.board[i][row] != self.last_token_placed:
-                    break
+        for i in range(self.height):
+            if vertical_count < 4 and self.board[column][i] != self.last_token_placed:
+                vertical_count = 0
             else:
+                vertical_count += 1
+
+            if vertical_count >= 4:
                 return True
 
-        # Check horizontal right
-        if column + 4 < self.width + 1:
-            for i in range(column, column + 4):
-                if self.board[i][row] != self.last_token_placed:
-                    break
+            if (column - row + i) in range(self.width) and diagonal_right_count < 4 and self.board[column - row + i][
+                i] != self.last_token_placed:
+                diagonal_right_count = 0
+            elif (column - row + i) in range(self.width):
+                diagonal_right_count += 1
+
+            if diagonal_right_count >= 4:
+                return True
+
+            if (column + row - i) in range(self.width) and diagonal_left_count < 4 and self.board[column + row - i][
+                i] != self.last_token_placed:
+                diagonal_right_count = 0
+            elif (column + row - i) in range(self.width):
+                diagonal_left_count += 1
+
+            if diagonal_left_count >= 4:
+                return True
+
+        for i in range(self.width):
+            if horizontal_count < 4 and self.board[i][row] != self.last_token_placed:
+                horizontal_count = 0
             else:
-                return True
+                horizontal_count += 1
 
-        # Check vertical down
-        if row + 4 < self.height + 1:
-            for i in range(row, row + 4):
-                if self.board[column][i] != self.last_token_placed:
-                    break
-            else:
-                return True
-
-        # Check diagonal left
-        if column - 4 > -2 and row + 4 < self.height + 1:
-            count_token_equals = 0
-            for i in range(column, column - 4, -1):
-                for j in range(row, row + 4):
-                    count_token_equals += self.board[i][j] == self.last_token_placed and i - column == (j - row) * -1
-
-            if count_token_equals == 4:
-                return True
-
-        # Check diagonal right
-        if column + 4 < self.width + 1 and row + 4 < self.height + 1:
-            count_token_equals = 0
-            for i in range(column, column + 4):
-                for j in range(row, row + 4):
-                    count_token_equals += self.board[i][j] == self.last_token_placed and i - column == j - row
-
-            if count_token_equals == 4:
+            if horizontal_count >= 4:
                 return True
 
         return False
@@ -145,15 +128,28 @@ class ConnectFourBoard:
                 return TokenEnum.RED
 
     def generate_all_children(self) -> Generator["ConnectFourBoard", None, None]:
+        if self.is_winning:
+            return
         yield from (self.drop_token(i, self._select_token()) for i in range(self.width) if not self.is_column_full[i])
 
-    def who_won(self) -> int:
+    def who_won(self) -> float:
         win = self.is_last_move_winning()
 
         if win:
-            return win * 1 if self.last_token_placed == TokenEnum.YELLOW else -1
+            win *= 1 if self.last_token_placed == TokenEnum.YELLOW else -1
+            self.weight = win
+            self.is_winning = True
+            return win
 
-        return 0
+        return self.weight
+
+    def __eq__(self, other: "ConnectFourBoard") -> bool:
+        for i in range(self.width):
+            for j in range(self.height):
+                if self.board[i][j] != other.board[i][j]:
+                    return False
+
+        return True
 
 
 @dataclass
@@ -182,18 +178,60 @@ def build_tree(root: Node, depth: int = 0) -> Node:
     return root
 
 
+def get_all_tree_leafs(root: Node) -> list[Node]:
+    stack = [root]
+
+    leafs = []
+
+    while stack:
+        top = stack.pop()
+
+        if not top.children:
+            leafs.append(top)
+
+        stack += top.children
+
+    return leafs
+
+
+def calculate_tree_weight(root: Node) -> Node:
+    if not root.children:
+        root.value.who_won()
+        return root
+
+    for child in root.children:
+        calculate_tree_weight(child)
+
+        root.value.weight += child.value.weight
+
+    root.value.weight /= len(root.children)
+
+    return root
+
+
 def print_tree(root: Node) -> None:
     stack = [root]
 
     while stack:
         top = stack.pop()
-        print("    " * top.depth + str(top.value.__repr__()))
+        print("    " * top.depth + str(top.depth) + " " + str(top.value.__repr__()))
 
         stack += top.children
 
 
-board = ConnectFourBoard(7, 6)
+board = ConnectFourBoard(15, 15)
+
+board = board.drop_token(0, TokenEnum.RED)
+board = board.drop_token(1, TokenEnum.YELLOW)
+board = board.drop_token(1, TokenEnum.RED)
+board = board.drop_token(2, TokenEnum.YELLOW)
+board = board.drop_token(2, TokenEnum.RED)
+board = board.drop_token(3, TokenEnum.YELLOW)
+board = board.drop_token(3, TokenEnum.RED)
 
 root = Node(board)
-root = build_tree(root, 2)
-print_tree(root)
+root = build_tree(root, 3)
+root = calculate_tree_weight(root)
+print(max(i.value.weight for i in root.children))
+print(*(i.value.weight for i in root.children))
+# print_tree(root)
